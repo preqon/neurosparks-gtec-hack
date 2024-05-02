3812f4f48005e499f41c8ed1b57be2a0279cdccc
recoded-13 <102366878+recoded-13@users.noreply.github.com>
2024-05-02 10:25:14 +0500

    preprocess.m

--- preprocess.m
+++ preprocess.m
@@ -1,2 +1,7 @@
 // this matlab script applies a bandpass filter between 8 and 30 Hz 
+eegData=load('P1_pre_training.mat') // P1_pre_training.mat is substituted with other file names
+samplingRate = 256;
+[b, a] = butter(4, [8 30] / (fs / 2), 'bandpass'); % Butterworth filter
+filteredData = filter(b, a, y')';
+
 // to all participant/treatments
